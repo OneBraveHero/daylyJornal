df8486b60c0f400508c157613dd48cc6ee6e9665
Anthony Diaz <anthonycrazycuba@gmail.com>
2023-02-27 16:29:29 -0500

    first-commit

---
+++ app.js
@@ -0,0 +1,101 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const app = express();
+// Mongo Connect!!!!
+mongoose.connect('mongodb://127.0.0.1:27017/journal');
+
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+const postSchema = new Schema({
+    title: String,
+    body: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+const firstPost = new Post({
+    title:"Welcome to Your Journal",
+    body:"Feel free to write your day in here!"
+});
+
+
+app.get("/", function (req, res) {
+
+    Post.find(function (err,posts){
+        if (!err){
+            res.render("home", {
+                startingContent: homeStartingContent,
+                posts: posts,
+            });
+
+        }
+    });
+
+});
+
+app.get("/about", function (req, res) {
+    res.render("about", {startingAbout: aboutContent});
+});
+
+app.get("/contact", function (req, res) {
+    res.render("contact", {startingContact: contactContent});
+});
+
+app.get("/compose", function (req, res) {
+    res.render("compose");
+
+
+});
+
+app.get('/posts/:rail', function (req, res) {
+
+    const lupe = _.lowerCase(req.params.rail);
+
+    Post.find(function (err,posts) {
+        if (!err){
+            posts.forEach(function (post) {
+                const clue = _.lowerCase(post.title);
+
+                if (clue === lupe) {
+
+                    res.render("post", {lupe: lupe,
+                        title:post.title ,
+                        body:post.body });
+
+                }
+            });
+        }
+    });
+
+});
+app.post("/compose", function (req, res) {
+
+
+    const mongoPost = new Post({
+        title:req.body.postTitle,
+        body:req.body.postBody
+    });
+
+    mongoPost.save();
+    res.redirect("/");
+});
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+} );
+
